ac6dd97ac5db4cb969d996385f93b4a4a36ce12e
Anthony Eden <anthonyeden@gmail.com>
2008-05-25 17:09:22 -0400

    status code work

--- lib/rsaml/protocol/status_code.rb
+++ lib/rsaml/protocol/status_code.rb
@@ -1,5 +1,9 @@
-module RSAML
-  module Protocol
+module RSAML #:nodoc:
+  module Protocol #:nodoc:
+    # A code or a set of nested codes representing the status of the corresponding request.
+    # 
+    # More information on available status codes may be found in Section 3.2.2.2 of the SAML 2.0 Core
+    # specification.
     class StatusCode
       # Initialize the status code with the given value
       def initialize(value)
@@ -20,7 +24,7 @@ module RSAML
       
       # Hash of symbol/StatusCode pairs representing top-level status codes.
       def self.top_level_status_codes
-        {
+        @top_level_status_codes ||= {
           :success => SUCCESS,
           :requestor => REQUESTOR,
           :responder => RESPONDER,
@@ -28,8 +32,29 @@ module RSAML
         }
       end
       
+      # Hash of symbol/StatusCode pairs representing second-level status codes.
       def self.second_level_status_codes
-        
+        @second_level_status_codes ||= {
+          :authn_failed => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:AuthnFailed'),
+          :invalid_attr_name_or_value => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:InvalidAttrNameOrValue'),
+          :invalid_name_id_policy => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:InvalidNameIDPolicy'),
+          :no_authn_context => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:NoAuthnContext'),
+          :no_available_idp => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:NoAvailableIDP'),
+          :no_passive => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:NoPassive'),
+          :no_supported_idp => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:NoSupportedIDP'),
+          :partial_logout => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:PartialLogout'),
+          :proxy_count_exceeded => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:ProxyCountExceeded'),
+          :request_denied => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:RequestDenied'),
+          :request_unsupported => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:RequestUnsupported'),
+          :request_version_deprecated => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:RequestVersionDeprecated'),
+          :request_version_too_high => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:RequestVersionTooHigh'),
+          :request_version_too_low => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:RequestVersionTooLow'),
+          :resource_not_recognized => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:ResourceNotRecognized'),
+          :too_many_responses => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:TooManyResponse'),
+          :unknown_attr_profile => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:UnknownAttrProfile'),
+          :unknown_principal => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:UnknownPrincipal'),
+          :unsupported_binding => StatusCode.new('urn:oasis:names:tc:SAML:2.0:status:UnsupportedBinding'),
+        }
       end
       
       # The status code value. Value is a URI reference.
@@ -37,6 +62,10 @@ module RSAML
       
       # An optional child status code.
       attr_accessor :status_code
+      
+      def validate
+        raise ValidationError, "Value is required" if value.nil?
+      end
       
       # Construct an XML fragment representing the request
       def to_xml(xml=Builder::XmlMarkup.new)
--- test/protocol/status_code_test.rb
+++ test/protocol/status_code_test.rb
@@ -4,8 +4,31 @@ class StatusCodeTest < Test::Unit::TestCase
   include RSAML::Protocol
   
   context "the StatusCode class" do
-    should "have 4 top level status codes" do
+    should "have 4 top-level status codes" do
       assert_equal 4, StatusCode.top_level_status_codes.length
+    end
+    should "have 19 second-level status codes" do
+      assert_equal 19, StatusCode.second_level_status_codes.length
+    end
+    should "have constants for the top-level status codes" do
+      assert_equal StatusCode.top_level_status_codes[:success], StatusCode::SUCCESS
+      assert_equal StatusCode.top_level_status_codes[:requestor], StatusCode::REQUESTOR
+      assert_equal StatusCode.top_level_status_codes[:responder], StatusCode::RESPONDER
+      assert_equal StatusCode.top_level_status_codes[:version_mismatch], StatusCode::VERSION_MISMATCH
+    end
+  end
+  
+  context "a success status code instance" do
+    setup do
+      @status_code = StatusCode::SUCCESS
+    end
+    context "when producing xml" do
+      should "have the samlp:StatusCode element name" do
+        assert_match(/<samlp:StatusCode/, @status_code.to_xml)
+      end
+      should "include a value" do
+        assert_match(/Value="urn:oasis:names:tc:SAML:2.0:status:Success"/, @status_code.to_xml)
+      end
     end
   end
 end
